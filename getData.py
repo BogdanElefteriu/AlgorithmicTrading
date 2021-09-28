a16fc90d2c2e2ce78d2a19ede6c3671e23c26129
Artur112 <artur.jurgenson@hotmail.com>
2021-09-28 19:47:41 +0100

    Cleaned Up getData File

--- getData.py
+++ getData.py
@@ -1,63 +1,50 @@
-# IMPORTS
 import pandas as pd
 import math
 import os.path
 from binance.client import Client
-from datetime import timedelta, datetime
-from dateutil import parser
+from datetime import timedelta, datetime, date
 
-### API
-binance_api_key = 'c4aqjt0eGrzPFPh8QgggNZ3UvNz0Xq0eG4UE5Y6Cy8V89tcw5cPAmrgxaa41Cl57'    #Enter your own API-key here
-binance_api_secret = 'CwTBAKDnC4I3MyCq0elzxxNrR21IbaRYEtiDVrIW74skGcQjLNwqjdEbaxVkSsow' #Enter your own API-secret here
+binance_api_key = 'c4aqjt0eGrzPFPh8QgggNZ3UvNz0Xq0eG4UE5Y6Cy8V89tcw5cPAmrgxaa41Cl57'
+binance_api_secret = 'CwTBAKDnC4I3MyCq0elzxxNrR21IbaRYEtiDVrIW74skGcQjLNwqjdEbaxVkSsow'
 
-### CONSTANTS
-binsizes = {"1m": 1, "5m": 5, '30m':30, "1h": 60, "1d": 1440}
+kline_sizes = {"1m": 1, "5m": 5, '30m': 30, "1h": 60, "1d": 1440}
 batch_size = 750
 binance_client = Client(api_key=binance_api_key, api_secret=binance_api_secret)
 
-### FUNCTIONS
-def minutes_of_new_data(symbol, kline_size, data, source):
+def download_binance_data(symbol, kline_size, start_date='1 Jan 2017'):
+    """
+        Function that downloads raw data from binance API and saves to file. If file already exists, fetches
+        and appends new data.
+        - symbol: Ticker Symbol
+        - kline_size: Size of Klines
+        - start_date: optional start date
+       :return:
+    """
+    if not os.path.exists('./data'):
+        os.mkdir('./data')
+    if not os.path.exists('./data/raw'):
+        os.mkdir('./data/raw')
+
+    filename = './data/raw/%s-%s-raw-data.csv' % (symbol, kline_size)
+    data = pd.read_csv(filename, index_col=0, header=0) if os.path.isfile(filename) else pd.DataFrame()
+    start_time = datetime.strptime(start_date, '%d %b %Y')
+    today = date.today()
+    end_time = datetime(today.year, today.month, today.day)
+
     if len(data) > 0:
-        old = parser.parse(data["timestamp"].iloc[-1])
+        start_time = datetime.strptime(data.iloc[-1].name, '%Y-%m-%d %H:%M:%S') + timedelta(minutes=kline_sizes[kline_size])
 
-    elif source == "binance":
-        old = datetime.strptime('1 Jan 2017', '%d %b %Y')
+    if start_time < end_time:
+        delta_min = (end_time - start_time).total_seconds()/60
+        available_data = math.ceil(delta_min/kline_sizes[kline_size])
+        print('Downloading %d instances of %s data since %s' % (available_data, kline_size, start_time))
 
-    if source == "binance":
-        new = pd.to_datetime(binance_client.get_klines(symbol=symbol, interval=kline_size)[-1][0], unit='ms')
-    return old, new
-
-def Get_binance(symbol, kline_size, save = False):
-    filename = './data/%s-%s-data.csv' % (symbol, kline_size)
-
-    if os.path.isfile(filename):
-        data_df = pd.read_csv(filename)
-    else:
-        data_df = pd.DataFrame()
-
-    oldest_point, newest_point = minutes_of_new_data(symbol, kline_size, data_df, source = "binance")
-    delta_min = (newest_point - oldest_point).total_seconds()/60
-    available_data = math.ceil(delta_min/binsizes[kline_size])
-
-    if oldest_point == datetime.strptime('1 Jan 2017', '%d %b %Y'):
-        print('Downloading all available %s data for %s. Be patient..!' % (kline_size, symbol))
-    else:
-        print('Downloading %d minutes of new data available for %s, i.e. %d instances of %s data.' % (delta_min, symbol, available_data, kline_size))
-
-    klines = binance_client.get_historical_klines(symbol, kline_size, oldest_point.strftime("%d %b %Y %H:%M:%S"), newest_point.strftime("%d %b %Y %H:%M:%S"))
-    data = pd.DataFrame(klines, columns = ['timestamp', 'open', 'high', 'low', 'close', 'volume', 'close_time', 'quote_av', 'trades', 'tb_base_av', 'tb_quote_av', 'ignore' ])
-    data['timestamp'] = pd.to_datetime(data['timestamp'], unit='ms')
-
-    if len(data_df) > 0:
-        temp_df = pd.DataFrame(data)
-        data_df = data_df.append(temp_df)
-    else:
-        data_df = data
-
-    data_df.set_index('timestamp', inplace=True)
-
-    if save:
-        data_df.to_csv(filename)
+        klines = binance_client.get_historical_klines(symbol, kline_size, start_time.strftime("%d %b %Y %H:%M:%S"), end_time.strftime("%d %b %Y %H:%M:%S"))
+        data_new = pd.DataFrame(klines, columns=['timestamp', 'open', 'high', 'low', 'close', 'volume', 'close_time', 'quote_av', 'trades', 'tb_base_av', 'tb_quote_av', 'ignore' ])
+        data_new['timestamp'] = pd.to_datetime(data_new['timestamp'], unit='ms')
+        data_new.set_index('timestamp', inplace=True)
+        data = data.append(data_new, sort=True)
+        data.to_csv(filename)
 
     print('All caught up..!')
-    return data_df
+    return
